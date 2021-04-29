5a7b2075c2513b338c2f870cf7ea045069d12e7b
PeterDaddy <78677453+PeterDaddy@users.noreply.github.com>
2021-04-29 17:46:10 +0900

    Add files via upload

--- libraries/intra_prediction_9_modes.m
+++ libraries/intra_prediction_9_modes.m
@@ -10,7 +10,7 @@ classdef intra_prediction_9_modes
                end
             end
         end
-        function out=horizonatal_replication(L,N)
+        function out=horizonatal_replication(L,N)   
             for i=1:N
                 for j=1:N
                    out(i,j)=L(i);  % Horizonatal Replication
@@ -20,7 +20,7 @@ classdef intra_prediction_9_modes
         function out=mean_dc(L,T,N)
             for i=1:N
                 for j=1:N
-                    out(i,j)=round(mean([L(1:N) T(1:N) 4]));  % Mean / DC
+                    out(i,j)= round(mean([L(1:N) T(1:N) 4]));  % Mean / DC
                 end
             end
         end
@@ -32,7 +32,7 @@ classdef intra_prediction_9_modes
          d = (T(4) + 2*T(5) + T(6) + 2) / 4;
          e = (T(5) + 2*T(6) + T(7) + 2) / 4;
          f = (T(6) + 2*T(7) + T(8) + 2) / 4;
-         g = (T(7) + 3*T(8)      + 2) / 4;
+         g = (T(7) + 3*T(8)        + 2) / 4;
 
          out(1,1)=a;out(1,2)=b;out(1,3)=c;out(1,4)=d;
          out(2,1)=b;out(2,2)=c;out(2,3)=d;out(2,4)=e;
@@ -61,9 +61,9 @@ classdef intra_prediction_9_modes
         %diagonal Down right
          a = (L(4) + 2*L(3) + L(2) + 2) / 4;
          b = (L(3) + 2*L(2) + L(1) + 2) / 4;
-         c = (L(2) + 2*L(1) + LT + 2) / 4;
-         d = (L(1) + 2*LT + T(1) + 2) / 4;
-         e = (LT + 2*T(1) + T(2) + 2) / 4;
+         c = (L(2) + 2*L(1) + LT   + 2) / 4;
+         d = (L(1) + 2*LT   + T(1) + 2) / 4;
+         e = (LT   + 2*T(1) + T(2) + 2) / 4;
          f = (T(1) + 2*T(2) + T(3) + 2) / 4;
          g = (T(1) + 2*T(3) + T(4) + 2) / 4;
 
@@ -76,15 +76,15 @@ classdef intra_prediction_9_modes
 
         function out=vertical_right(L,T,LT)
         %vertical right
-         a = (LT + T(1) + 1) / 2;
-         b = (T(1) + T(2) + 1) / 2;
-         c = (T(2) + T(3) + 1) / 2;
-         d = (T(3) + T(4) + 1) / 2;
-         e = (L(1) + 2*LT + T(1) + 2) / 4;
-         f = (LT + 2*T(1) + T(2) + 2) / 4;
+         a = (LT   + T(1)   + 1) / 2;
+         b = (T(1) + T(2)   + 1) / 2;
+         c = (T(2) + T(3)   + 1) / 2;
+         d = (T(3) + T(4)   + 1) / 2;
+         e = (L(1) + 2*LT   + T(1) + 2) / 4;
+         f = (LT   + 2*T(1) + T(2) + 2) / 4;
          g = (T(1) + 2*T(2) + T(3) + 2) / 4;
          h = (T(2) + 2*T(3) + T(4) + 2) / 4;
-         i = (LT + 2*L(1) + L(2) + 2) / 4;
+         i = (LT   + 2*L(1) + L(2) + 2) / 4;
          j = (L(1) + 2*L(2) + L(3) + 2) / 4;
 
          out(1,1)=a;out(1,2)=b;out(1,3)=c;out(1,4)=d;
@@ -96,12 +96,12 @@ classdef intra_prediction_9_modes
 
         function out=horizontal_down(L,T,LT)
         %Horizontal Down
-         a = (LT + L(1) + 1) / 2;
-         b = (L(1) + 2*LT + T(1) + 2) / 4;
-         c = (LT + 2*T(1) + T(2) + 2) / 4;
+         a = (LT   + L(1)   + 1) / 2;
+         b = (L(1) + 2*LT   + T(1) + 2) / 4;
+         c = (LT   + 2*T(1) + T(2) + 2) / 4;
          d = (T(1) + 2*T(2) + T(3) + 2) / 4;
-         e = (L(1) + L(2) + 1) / 2;
-         f = (LT + 2*L(1) + L(2) + 2) / 4;
+         e = (L(1) + L(2)   + 1) / 2;
+         f = (LT   + 2*L(1) + L(2) + 2) / 4;
          g = (L(2) + L(3) + 1) / 2;
          h = (L(1) + 2*L(2) + L(3) + 2) / 4;
          i = (L(3) + L(4) + 1) / 2;
@@ -115,11 +115,11 @@ classdef intra_prediction_9_modes
         end
         function out=vertical_left(T)
         %Vertical Left
-         a = (T(1) + T(2) + 1) / 2;
-         b = (T(2) + T(3) + 1) / 2;
-         c = (T(3) + T(4) + 1) / 2;
-         d = (T(4) + T(5) + 1) / 2;
-         e = (T(5) + T(6) + 1) / 2;
+         a = (T(1) + T(2)   + 1) / 2;
+         b = (T(2) + T(3)   + 1) / 2;
+         c = (T(3) + T(4)   + 1) / 2;
+         d = (T(4) + T(5)   + 1) / 2;
+         e = (T(5) + T(6)   + 1) / 2;
          f = (T(1) + 2*T(2) + T(3) + 2) / 4;
          g = (T(2) + 2*T(3) + T(4) + 2) / 4;
          h = (T(3) + 2*T(4) + T(5) + 2) / 4;
@@ -136,17 +136,16 @@ classdef intra_prediction_9_modes
         %Horizontal UP
          a = (L(1) + L(2) + 1) / 2;
          b = (L(1) + 2*L(2) + L(3) + 2) / 4;
-         c = (L(2) + L(3) + 1) / 2;
+         c = (L(2) + L(3)   + 1) / 2;
          d = (L(2) + 2*L(3) + L(4) + 2) / 4;
-         e = (L(3) + L(4) + 1) / 2;
-         f = (L(3) + 3*L(4)      + 2) / 4;
+         e = (L(3) + L(4)   + 1) / 2;
+         f = (L(3) + 3*L(4) + 2) / 4;
          g = L(4);
 
          out(1,1)=a;out(1,2)=b;out(1,3)=c;out(1,4)=d;
          out(2,1)=c;out(2,2)=d;out(2,3)=e;out(2,4)=f;
          out(3,1)=e;out(3,2)=f;out(3,3)=g;out(3,4)=g;
          out(4,1)=g;out(4,2)=g;out(4,3)=g;out(4,4)=g;
-
         end
         
         function intra_prediction_candidate=sum_of_absolute_difference(c1,c2,c3,c4,c5,c6,c7,c8,c9,sub_frame_temp)
@@ -164,40 +163,40 @@ classdef intra_prediction_9_modes
             [~,min_SAD]=min(SAD);
             switch min_SAD
                 case 1
-                   fprintf('Vertical Replication \n');
-                    intra_prediction_candidate=c1;
+                    fprintf('Vertical Replication \n');
+                    intra_prediction_candidate=floor(c1);
                     intra_mode='Mode 1';
                 case 2
-                   fprintf('Horizonatal Replication \n');
-                    intra_prediction_candidate=c2;
+                    fprintf('Horizonatal Replication \n');
+                    intra_prediction_candidate=floor(c2);
                     intra_mode='Mode 2';
                 case 3
                     fprintf('Mean/DC \n');
-                    intra_prediction_candidate=c3;
+                    intra_prediction_candidate=floor(c3);
                     intra_mode='Mode 3';
                 case 4
                     fprintf('Diagonal Down-Left \n');
-                    intra_prediction_candidate=c4;
+                    intra_prediction_candidate=floor(c4);
                     intra_mode='Mode 4';
                 case 5
                     fprintf('Diagonal Down-Right \n');
-                    intra_prediction_candidate=c5;
+                    intra_prediction_candidate=floor(c5);
                     intra_mode='Mode 5';
                 case 6
                     fprintf('Vertical Right \n');
-                    intra_prediction_candidate=c6;
+                    intra_prediction_candidate=floor(c6);
                     intra_mode='Mode 6';
                 case 7
                     fprintf('Horizontal Down \n');
-                    intra_prediction_candidate=c7;
+                    intra_prediction_candidate=floor(c7);
                     intra_mode='Mode 7';
                 case 8
                     fprintf('Vertical Left \n');
-                    intra_prediction_candidate=c8;
+                    intra_prediction_candidate=floor(c8);
                     intra_mode='Mode 8';
                 case 9
                     fprintf('Horizontal Up \n');
-                    intra_prediction_candidate=c9;
+                    intra_prediction_candidate=floor(c9);
                     intra_mode='Mode 9';
             end
         end
